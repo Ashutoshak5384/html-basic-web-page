b6fd2131696f6baa7b725bcbea74d2b0eae631ca
ayyushkuu <75512011+ayyushkuu@users.noreply.github.com>
2022-10-21 16:48:13 +0530

    Create Quiksort.js

---
+++ Quiksort.js
@@ -0,0 +1,23 @@
+//here is a code for quick sort using java script 
+const quickSort = arr => {
+  const a = [...arr];
+  if (a.length < 2) return a;
+  const pivotIndex = Math.floor(arr.length / 2);
+  const pivot = a[pivotIndex];
+  const [lo, hi] = a.reduce(
+    (acc, val, i) => {
+      if (val < pivot || (val === pivot && i != pivotIndex)) {
+        acc[0].push(val);
+      } else if (val > pivot) {
+        acc[1].push(val);
+      }
+      return acc;
+    },
+    [[], []]
+  );
+  return [...quickSort(lo), pivot, ...quickSort(hi)];
+};
+ 
+console.log(quickSort([2, 9, 6, 2, 6, 8, 1, 7]));
+
+//output : 1, 2, 2, 6, 6, 7, 8, 9
