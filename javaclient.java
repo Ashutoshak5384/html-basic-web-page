c708db8ef7dd4a71b875a3c160b1d6ccc9e8ff40
ritikyd <116355951+ritikyd@users.noreply.github.com>
2022-10-21 17:07:36 +0530

    Create javaclient.java

---
+++ javaclient.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.net.*;
+
+class Client
+
+{
+	public static void main(String[] args)throws Exception{
+		Socket s = new Socket("localhost" ,777);
+		InputStream obj = s.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(obj));
+		String str;
+		while((str = br.readLine()) != null)
+			System.out.println("From server "+str);
+		br.close();
+		s.close();
+
+	}
+}
