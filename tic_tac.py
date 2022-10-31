5bd88cb34cb2b65327a7b202df5f9c0e99fbfbf4
sitgdsc2022 <114358500+sitgdsc2022@users.noreply.github.com>
2022-10-31 16:33:10 +0530

    Create tic_tac.py

---
+++ tic_tac.py
@@ -0,0 +1,30 @@
+
+# ...
+
+class TicTacToeBoard(tk.Tk):
+    # ...
+
+    def _create_board_grid(self):
+        grid_frame = tk.Frame(master=self)
+        grid_frame.pack()
+        for row in range(3):
+            self.rowconfigure(row, weight=1, minsize=50)
+            self.columnconfigure(row, weight=1, minsize=75)
+            for col in range(3):
+                button = tk.Button(
+                    master=grid_frame,
+                    text="",
+                    font=font.Font(size=36, weight="bold"),
+                    fg="black",
+                    width=3,
+                    height=2,
+                    highlightbackground="lightblue",
+                )
+                self._cells[button] = (row, col)
+                button.grid(
+                    row=row,
+                    column=col,
+                    padx=5,
+                    pady=5,
+                    sticky="nsew"
+                )
