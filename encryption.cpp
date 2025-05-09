edd6da99b149d8b7856e96e6d0f67d3e51638e69
Kimathi Sedegah <sedkim370@gmail.com>
2025-05-10 00:05:19 +0100

    Update encryption.cpp

--- encryption.cpp
+++ encryption.cpp
@@ -2,48 +2,58 @@
 #include<fstream>
 #include<stdio.h>
 using namespace std;
+
 int main()
 {
-    char fileName[30], ch;
+    char fileName[100], ch;
     fstream fps, fpt;
-    cout<<"Enter the Name of File: ";
-    gets(fileName);
+
+    cout << "Enter the Name of File: ";
+    cin.getline(fileName, 100); // SAFER alternative to gets()
+
     fps.open(fileName, fstream::in);
-    if(!fps)
+    if (!fps)
     {
-        cout<<"\nError Occurred, Opening the Source File (to Read)!";
+        cout << "\nError Occurred, Opening the Source File (to Read)!";
         return 0;
     }
+
     fpt.open("tmp.txt", fstream::out);
-    if(!fpt)
+    if (!fpt)
     {
-        cout<<"\nError Occurred, Opening/Creating the tmp File!";
+        cout << "\nError Occurred, Opening/Creating the tmp File!";
         return 0;
     }
-    while(fps>>noskipws>>ch)
+
+    while (fps >> noskipws >> ch)
     {
-        ch = ch+100;
-        fpt<<ch;
+        ch = ch + 100; // basic encryption
+        fpt << ch;
     }
+
     fps.close();
     fpt.close();
+
     fps.open(fileName, fstream::out);
-    if(!fps)
+    if (!fps)
     {
-        cout<<"\nError Occurred, Opening the Source File (to write)!";
+        cout << "\nError Occurred, Opening the Source File (to write)!";
         return 0;
     }
+
     fpt.open("tmp.txt", fstream::in);
-    if(!fpt)
+    if (!fpt)
     {
-        cout<<"\nError Occurred, Opening the tmp File!";
+        cout << "\nError Occurred, Opening the tmp File!";
         return 0;
     }
-    while(fpt>>noskipws>>ch)
-        fps<<ch;
+
+    while (fpt >> noskipws >> ch)
+        fps << ch;
+
     fps.close();
     fpt.close();
-    cout<<"\nFile '"<<fileName<<"' Encrypted Successfully!";
-    cout<<endl;
+
+    cout << "\nFile '" << fileName << "' Encrypted Successfully!\n";
     return 0;
 }
